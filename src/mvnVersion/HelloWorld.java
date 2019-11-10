d811fa40c568cb89488be4577ee81c93268530f9
Nathan Bowman <nathan.daniel.bowman@gmail.com>
2019-11-10 15:16:55 -0600

    0.0.1 initial version

---
+++ src/mvnVersion/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mvnVersion;
+
+/**
+ * @author Bowman
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World 0.0.1");
+
+	}
+
+}
